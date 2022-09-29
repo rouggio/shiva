f3944c2a5336a4ff24299f542e74a71a6740c7cf
Dario Busco <dario.x.busco@gmail.com>
2022-09-29 23:13:52 +0200

    initial revision

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,10 +1,39 @@
-import { Context, Get, HttpResponseOK } from '@foal/core';
+import { Context, Delete, Get, HttpResponseCreated, HttpResponseNoContent, HttpResponseNotFound, HttpResponseOK, Post, ValidateBody, ValidatePathParam } from '@foal/core';
+import { Todo } from '../entities';
 
 export class ApiController {
 
-  @Get('/')
-  index(ctx: Context) {
-    return new HttpResponseOK('Hello world!');
+  @Get('/todos')
+  async getTodos() {
+    const todos = await Todo.find();
+    return new HttpResponseOK(todos);
+  }
+
+  @Post('/todos')
+  @ValidateBody({
+    additionalProperties: false,
+    properties: {
+      text: { type: 'string' }
+    },
+    required: [ 'text' ],
+    type: 'object'
+  })
+  async postTodo(ctx: Context) {
+    const todo = new Todo();
+    todo.text = ctx.request.body.text;
+    await todo.save();
+    return new HttpResponseCreated(todo);
+  }
+
+  @Delete('/todos/:id')
+  @ValidatePathParam('id', { type: 'number' })
+  async deleteTodo(ctx: Context) {
+    const todo = await Todo.findOne({ id: ctx.request.params.id });
+    if (!todo) {
+      return new HttpResponseNotFound();
+    }
+    await todo.remove();
+    return new HttpResponseNoContent();
   }
 
 }
