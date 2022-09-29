315eae7cb8a8db44ba0f7794ed0ea240c5229a4a
Dario Busco <dario.x.busco@gmail.com>
2022-09-29 22:53:48 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'source-map-support/register';
+
+// 3p
+import { Config, createApp, displayServerURL } from '@foal/core';
+
+// App
+import { AppController } from './app/app.controller';
+
+async function main() {
+  const app = await createApp(AppController);
+
+  const port = Config.get('port', 'number', 3001);
+  app.listen(port, () => displayServerURL(port));
+}
+
+main()
+  .catch(err => { console.error(err.stack); process.exit(1); });
